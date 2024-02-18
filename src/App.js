953875a3a1ba93088a100fb6504432492e244a83
karthikeyadondapati <2211CS020137@mallareddyuniversity.ac.in>
2024-02-18 14:14:40 +0530

    initialize repository

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { email, message } = event.target.elements;
+    console.log("Email submitted:", email.value);
+    console.log("Message submitted:", message.value);
+    email.value = "";
+    message.value = "";
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="content">
+          <h1>Welcome to Joy World School</h1>
+          <p>Explore the world of knowledge and joy!.....</p>
+          <div className="links">
+            <form onSubmit={this.handleSubmit}>
+              <label>Email:</label>
+              <input type="email" name="email" placeholder="Enter your email" className="email-input" required />
+              <br />
+              <label>Message:</label>
+              <textarea name="message" rows="4" placeholder="Enter your message" className="message-input" required></textarea>
+              <br />
+              <button type="submit" className="enter-button"><h1>Submit</h1></button>
+            </form>{" "}
+            <br />
+          </div>
+          <button className="explore-button">
+            <h1>Explore</h1>
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
